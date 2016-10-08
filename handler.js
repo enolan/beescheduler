4957a4066c956ffcaa0a18cb3fc5056db97ef317
Echo Nolan <echo@echonolan.net>
2016-10-08 15:26:39 -0700

    I used it for production and broke my roads
    This is on testing goals and hopefully works.

--- handler.js
+++ handler.js
@@ -2,23 +2,17 @@
 
 const beeminder = require('beeminder');
 const rqpr = require('request-promise-native');
-const strftime = require('strftime');
+const moment = require('moment');
 const _ = require('lodash/fp');
 
 const token = "wF7Lo63rZv8qSHxbL-kh";
 
-function addDate(date, days) {
-    let res = new Date(date);
-    res.setDate(res.getDate() + days);
-    return res;
-}
-
 function beeDateFormat(date) {
-    return strftime("%Y-%m-%d", date);
+    return date.format("YYYY-MM-DD", date);
 }
 
 function setRoad(goalName, roadAll) {
-    console.log("setRoad " + goalName);
+    console.log("setting " + goalName);
     console.log(roadAll);
     let opts = {
         uri: 'https://www.beeminder.com/api/v1/users/enolan/goals/' + goalName + '.json',
@@ -29,7 +23,6 @@ function setRoad(goalName, roadAll) {
             'roadall': roadAll
         }
     };
-    console.log(opts);
     return rqpr(opts);
 }
 
@@ -47,42 +40,54 @@ function getGoalPromise(bm, goalName) {
 
 function scheduleGoal(bm, goalName, schedule) {
     console.log("scheduleGoal " + goalName + " " + schedule);
-    let oneWeekOut = addDate(Date.now(), 7);
+    let oneWeekOut = moment().utcOffset(-4).set({
+        'hour': 12,
+        'minute': 0,
+        'second': 0,
+        'millisecond': 0
+    }).add(7, 'days');
     return getGoalPromise(bm, goalName).then(goalInfo => {
         console.log(goalInfo);
         let truncatedRoad =
-            goalInfo.roadall.filter(x => new Date(x[0] * 1000) < oneWeekOut).map(
-                x => [beeDateFormat(new Date(x[0] * 1000)), x[1], x[2]]);
+            goalInfo.roadall.map(x => [moment(x[0], "X"), x[1], x[2]])
+            .filter(x => x[0] < oneWeekOut).map(
+                x => [beeDateFormat(x[0]), x[1], x[2]]);
         let lastSegmentFull = goalInfo.fullroad[truncatedRoad.length];
         truncatedRoad.push([beeDateFormat(oneWeekOut), null, lastSegmentFull[2]]);
-        let oneWeekOutDay = oneWeekOut.getDay();
+        let oneWeekOutDay = oneWeekOut.day();
         let newSegment = [];
         for (let i = 0; i < 7; i++) {
-            let targetDate;
+            let targetDate = oneWeekOut.clone();
             if (oneWeekOutDay < i) {
-                targetDate = addDate(oneWeekOut, i - oneWeekOutDay);
+                targetDate.add(i - oneWeekOutDay, 'd');
             } else {
-                targetDate = addDate(oneWeekOut, i - oneWeekOutDay + 7);
+                targetDate.add(i - oneWeekOutDay + 7, 'd');
             }
             newSegment.push([beeDateFormat(targetDate), null, schedule[i]]);
         }
         newSegment.sort(); // this is done by comparing on string representations.
+        console.log("New segment:");
+        console.log(newSegment);
         let newRoadall = truncatedRoad.concat(newSegment);
-        console.log(newRoadall);
         return setRoad(goalName, newRoadall);
     });
 }
 // Sunday, Monday...
 
+// const sched = {
+//     idris: [0, 7, 7, 6, 0, 0, 0],
+//     survey: [0, 0, 0, 0, 8, 2, 0],
+//     moonshot: [0, 0, 0, 0, 0, 5, 0]
+// };
 const sched = {
-    test: [0, 0, 0, 0, 0, 20, 0],
-    testb: [1, 10, 1, 5, 1, 0, 4],
-    testc: [12, 2, 11, 0.3, 1, 5, 0]
+    test: [1, 2, 5, 0, 9, 0.3, 4],
+    testb: [5, 4, 11, 3, 8, 0, 2],
+    testc: [0, 0, 0, 6, 0, 0, 2]
+
 };
 
 module.exports.setsched = (event, context, cb) => {
     let bm = beeminder(token);
-    console.log("setsched");
     console.log(sched);
     Promise.all(_.map(ent => scheduleGoal(bm, ent[0], ent[1]), _.toPairs(sched))).then(
         val => {
